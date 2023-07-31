b36b86845e884ac5eea9df4f97edba53942a2b4b
stelloprint <58522233+stephenlprice@users.noreply.github.com>
2023-07-31 17:34:32 -0500

    typo

--- pages/index.js
+++ pages/index.js
@@ -9,9 +9,10 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>If I've given you any reason to
-        pay a visit, hopefully you may also find reasons to stay and
-        check this place out. It's my hangout in cyberspace. Welcome.</p>
+        <p>If I've given you any reason to pay a visit, 
+        hopefully you may also find reasons to stay and
+        check this place out. It's my hangout in cyberspace. 
+        Welcome.</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
